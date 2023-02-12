8525ce7f111a6b81cf617b9015742c66810de0ed
Kyle Lierer <liererkt@gmail.com>
2023-02-12 12:02:26 -0500

    Removed snow.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,11 @@
 import Body from '../components/Body';
 import Logo from '../components/Logo';
-import Snow from '../components/Snowfall';
 
 function App() {
   return (
     <div className="flex flex-col items-center min-h-screen justify-center pt-20 pb-20">
       <Logo />
       <Body />
-      <Snow />
     </div>
   );
 }
