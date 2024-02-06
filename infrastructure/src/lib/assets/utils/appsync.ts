768bec965352f22816e70cbc4d9d472b7aada152
Kyle Lierer <liererkt@gmail.com>
2024-02-06 12:24:06 -0500

    Package fixes.

--- infrastructure/src/lib/assets/utils/appsync.ts
+++ infrastructure/src/lib/assets/utils/appsync.ts
@@ -1,8 +1,8 @@
+import { SignatureV4 } from '@smithy/signature-v4';
+import { httpsClient } from './clients';
+import { HttpRequest } from '@smithy/protocol-http';
 import { Sha256 } from '@aws-crypto/sha256-js';
 import { defaultProvider } from '@aws-sdk/credential-provider-node';
-import { HttpRequest } from '@aws-sdk/protocol-http';
-import { SignatureV4 } from '@aws-sdk/signature-v4';
-import { httpsClient } from './clients';
 
 export interface RequestParams {
   config: {
